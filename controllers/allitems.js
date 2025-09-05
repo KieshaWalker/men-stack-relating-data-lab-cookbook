47ae90cbbbe4c5f612e45f680f750a827e599533
KieshaWalker <llcwalkerk@gmail.com>
2025-09-05 15:40:46 -0500

    working on links to edit now

--- controllers/allitems.js
+++ controllers/allitems.js
@@ -5,14 +5,55 @@ const User = require('../models/user.js');
 
 
 router.get('/', async (req, res) => {// items/items// landing view is just // any time you use the word use the /items everything follows that slash.
-    console.log("In all items route");
-    res.render('items/allitems.ejs');
+    try{
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('items/allitems.ejs' , { tuna: currentUser.foods, });
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
 });
 
 router.get('/add', async (req, res) => {
     console.log('in route')
     res.render('items/add.ejs')
 });
+
+router.get('/edit', async (req, res) => {
+     try{
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('items/edit.ejs' , { tuna: currentUser.foods, });
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+});
+
+
+
+router.put('/:userId', async (req, res) => {
+    console.log('testing')
+});
+
+
+
+router.post('/', async (req, res) => {
+    console.log(req.body)
+     try{
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.foods.push(req.body)
+        await currentUser.save();
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+    res.redirect("/items")
+});
+
+router.delete('/', async (req, res) => {})
+
+
+
 
 
 
